3d72898ca3974a1fc257f7025d13a9d069ee6643
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-04-06 10:48:54 -0400

    Handle adding transaction

--- script.js
+++ script.js
@@ -11,5 +11,21 @@ const dummyTransactions = [
   {id: 2, text: "Salary", amount: 700},
   {id: 3, text: "Book", amount: -20},
   {id: 4, text: "Trip", amount: -200},
-
 ];
+
+let transactions = dummyTransactions;
+
+// Add transaction to DOm list
+function addTransactionDOM(transaction) {
+  // Get sign 
+  const sign = transaction.amount < 0 ? '-' : '+';
+
+  const item = document.createElement('li');
+
+ // Add class based on value
+ item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+ item.innerHTML = `
+  ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span> <button class="delete-btn"></button>
+ `
+ list.appendChild(item);
+}
