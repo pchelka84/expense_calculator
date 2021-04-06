7035390c110170b74453fb6bfaf6b3f4e057a96e
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-04-06 10:42:39 -0400

    Select elements

---
+++ script.js
@@ -0,0 +1,15 @@
+const balance = document.getElementById('balance');
+const moneyPlus = document.getElementById('money-plus');
+const moneyMinus = document.getElementById('money-minus');
+const list = document.getElementById('list');
+const form = document.getElementById('form');
+const text = document.getElementById('text');
+const amount = document.getElementById('amount'); 
+
+const dummyTransactions = [
+  {id: 1, text: "Flower", amount: -30},
+  {id: 2, text: "Salary", amount: 700},
+  {id: 3, text: "Book", amount: -20},
+  {id: 4, text: "Trip", amount: -200},
+
+];
